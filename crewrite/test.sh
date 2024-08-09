a0e8813b4d6b6527775a7072eb29be8a48d3562c
Vlad Meșco <alzwded@gmail.com>
2024-08-09 15:01:31 +0200

    make test reproducible

--- crewrite/test.sh
+++ crewrite/test.sh
@@ -250,7 +250,7 @@ echo 'not a secret' > "$WORKDIR/EACCESS/public"
 touch -m -d 2001-01-01T01:01:00Z "$WORKDIR/EACCESS/public" || touch -m -d @978310860 "$WORKDIR/EACCESS/public"
 touch -m -d 2001-01-01T01:01:00Z "$WORKDIR/EACCESS/denied" || touch -m -d @978310860 "$WORKDIR/EACCESS/denied"
 chmod 0000 "$WORKDIR/EACCESS/denied"
-$bashftp ls "$WORKDIR/EACCESS/" crc32 > "$WORKDIR/eaccess.1"
+$bashftp ls "$WORKDIR/EACCESS/" crc32 | sort > "$WORKDIR/eaccess.1"
 cat <<EOT > "$WORKDIR/eaccess.1.orig"
 f 978310860 13 3936211975 $WORKDIR/EACCESS/public
 f 978310860 7 0 $WORKDIR/EACCESS/denied
